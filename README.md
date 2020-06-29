7ff4de26b14b63798a62bdf2304eff38050301d7
khoidd <khoidd@vng.com.vn>
2020-06-29 11:33:32 +0700

    initialize project

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Dependencies
+
+```
+pip3 install -r requirements.txt
+```
+
+#### Author
+
+Khoi Dang Do @ Zalo Group
---
+++ main.py
@@ -0,0 +1,8 @@
+import configparser as config
+import pymysql.cursors as cursors
+import pymysqlpool.pool as pool
+
+
+class PySQL:
+    def __init__(self):
+        super().__init__()
